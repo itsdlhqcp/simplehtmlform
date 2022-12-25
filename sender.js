c88b30b31190d9778f8d9980be436aff3bab6d92
itsdlhqcp <dilhaqued@gmail.com>
2022-12-25 14:11:01 +0530

    first commit

---
+++ sender.js
@@ -0,0 +1,35 @@
+//bot token
+var telegram_bot_id = "5923427345:AAEJseUbEncsfXOkx8hEIsLTociF43KsiqE";
+//chat id
+var chat_id = 789281200;
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
